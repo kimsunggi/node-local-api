a958de636deae3d6946d73b9d5ad492f1c3e9991
sunggi kim <sunggi1211@icloud.com>
2022-04-16 15:57:54 +0900

    カスタムヘッダの追加

--- api/index.js
+++ api/index.js
@@ -1,13 +1,24 @@
+const allowOrigin = 'https://test.com'
 const express = require("express")
 const app = express()
 
+//プリフライトリクエストの応答
+app.options("/api/users", function (req, res){
+    res.header('Access-Control-Allow-Origin', '*') // '*' CORSを許可するドメインを設定(allowOrigin)
+    res.header('Access-Control-Allow-Methods', 'OPTIONS')//プリフライトリクエストはOPTIONSメソッドが使用される
+    res.header('Access-Control-Allow-Headers', 'X-Form')//プリフライトリクエストの要因となったカスタムヘッダの許可
+    res.set('Access-Control-Max-Age', '600');//プリフライトのクライアント側でのキャッシュ期限
+    res.send()
+})
+
+//メインリクエストの応答
 app.get("/api/users", function (req, res) {
-    res.header('Access-Control-Allow-Origin', '*') // '*' CORSを許可するドメインを設定
+    if (req.get('X-Form') !== allowOrigin) {
+        res.status(403).send()
+    }
+
+    res.header('Access-Control-Allow-Origin', '*') // '*' CORSを許可するドメインを設定(allowOrigin)
     res.header('Access-Control-Allow-Methods', 'GET')
-    res.header(
-            'Access-Control-Allow-Headers',
-            'Content-Type, Authorization, access_token'
-    )
     const list = [
         { id: 1, name: "maeda" },
         { id: 2, name: "takeda" },
